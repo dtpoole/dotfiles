c0fa7f594e7d50c83223ca7541a516f4088adcf6
David Poole <dtpoole@gmail.com>
2014-12-22 15:33:03 -0500

    workaround for puppet exec

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ SCRIPT=`basename $0`
 DIR=$PWD
 cd $DIR
 
+if [ -z $HOME ]; then
+    echo "HOME is not set. puppet?"
+    U=`whoami`
+    HOME=`grep $U /etc/passwd | awk -F":" '{ print $6 }'`
+    echo "Using $HOME..."
+fi
+
 echo Creating links...
 for FILE in *
 do
