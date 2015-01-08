2c01402c37c009933e9b2e706a29c3fb0da33a53
David Poole <dtpoole@gmail.com>
2015-01-08 13:25:01 -0500

    Remove puppet workaround. cleanup.

--- install.sh
+++ install.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 
-SCRIPT=`basename $0`
+SCRIPT=$(basename "$0")
 DIR=$PWD
-cd $DIR
-
-if [ -z $HOME ]; then
-    echo "HOME is not set. puppet?"
-    U=`whoami`
-    HOME=`grep $U /etc/passwd | awk -F":" '{ print $6 }'`
-    echo "Using $HOME..."
-fi
+cd "$DIR"
 
 echo Creating links...
 for FILE in *
@@ -18,12 +11,13 @@ do
         continue
     fi
 
-    echo -- $FILE
-    DEST=${HOME}/.${FILE}
-    if [ -e $DEST ] || [ -L $DEST ]; then
-        echo $DEST exists.
+    echo "-- $FILE"
+
+    DEST="$HOME/.$FILE"
+    if [ -e "$DEST" ] || [ -L "$DEST" ]; then
+        echo "$DEST exists."
     else
-        ln -s $DIR/$FILE $DEST
+        ln -s "$DIR/$FILE" "$DEST"
     fi
 done
 
--- setup_vim.sh
+++ setup_vim.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ ! -d ~/.vim/bundle/vundle ]; then
     echo Setting up VIM...
